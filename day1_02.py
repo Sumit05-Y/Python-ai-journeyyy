fefca19068e53a2a596e9d4472610a2cc71692d8
SUMIT <sahsumit047@gmail.com>
2026-05-20 14:18:39 +0545

    PRIME COMPOSITE

---
+++ day1_02.py
@@ -0,0 +1,11 @@
+a = int(input("ENTER A NUMBER"))
+b = 0
+
+for i in range(1, a + 1):
+    if a % i == 0:
+        b += 1
+
+if b == 2:
+    print("prime")
+else:
+    print("composite")
---
+++ pythonday1.py
@@ -0,0 +1,8 @@
+name = "sumit sah"
+age = 17
+height=6.2
+is_student=True
+middle_name=None
+frequency=3+4j
+for v in [name , age , height , is_student , middle_name , frequency]:
+  print(f"Value:  {v!r:<20}Type:{type(v).__name__}")
