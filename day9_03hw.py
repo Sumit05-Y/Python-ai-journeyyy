f954eaa92fd9c71db7eda770e85c99b744e1c983
SUMIT <sahsumit047@gmail.com>
2026-06-04 00:19:18 +0545

    CODE FOR IT

--- day9_03hw.py
+++ day9_03hw.py
@@ -4,4 +4,18 @@
 #return true if the username is availabe
 #return false if the username is already taken(already in the set)
 
+def is_username_available(existing_usernames,new_username):
+    if existing_usernames == new_username:
+        print("Username unavailable")
+    else:
+        print("Username available")
+existing_usernames = {"Ram","Hari","Sita","Geeta","Shyam"}
+new_username_byuser = input("Enter username :\n")
+new_username = existing_usernames.copy()
+new_username.add(new_username_byuser)
+
+is_username_available(existing_usernames,new_username)
+
+
+
 
