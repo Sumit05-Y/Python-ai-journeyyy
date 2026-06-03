c960d9e58cd78f88e84af43a26dccaf1c0d279a4
SUMIT <sahsumit047@gmail.com>
2026-06-03 23:38:23 +0545

    USERNAME EXISTS OR NOT LOGIC QUES

---
+++ day9_03hw.py
@@ -0,0 +1,7 @@
+#Write a python function called is_username_available that checks if a new username can be used
+#convert the list of existing_usernames into a set inside your function to ensure a fast lookup.
+#check if the new_username exists in that set
+#return true if the username is availabe
+#return false if the username is already taken(already in the set)
+
+
