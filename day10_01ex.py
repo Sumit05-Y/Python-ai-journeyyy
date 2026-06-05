2f1e93619378b0c22caee486ea7d96c66e85d4b7
SUMIT <sahsumit047@gmail.com>
2026-06-05 23:37:08 +0545

    New code

--- day10_01ex.py
+++ day10_01ex.py
@@ -1,4 +1,12 @@
-python_student ={"Ram","Sita","Hari","Shyam","Geeta"}
-ml_student= {"ABC1","DEF2","GHI3","Hari","Sita"}
-both = python_student & ml_student
-print(both)
+#python_student ={"Ram","Sita","Hari","Shyam","Geeta"}
+#ml_student= {"ABC1","DEF2","GHI3","Hari","Sita"}
+#both = python_student & ml_student
+#print(both)
+tag = {"python","ai"}
+tag.add("ml")
+print(tag)
+#tag.remove("ml")
+print(tag)
+tag.discard("java")
+print(tag)
+print("python" in tag)
