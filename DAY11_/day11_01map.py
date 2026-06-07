b6b6b0bc4b046d51ce7a59209b37eb034c7e94a4
SUMIT <sahsumit047@gmail.com>
2026-06-07 23:28:49 +0545

    map function

---
+++ DAY11_/day11_01map.py
@@ -0,0 +1,6 @@
+#map
+number = [1,2,3,4,5]
+def double(x):
+    return x * 2
+result = list(map(double,number)) #map ley double ma number ma bhako list ko sab number ewta ewta gari pathaunxa
+print(result)
---
+++ DAY11_/day11_02mapwithlambda.py
@@ -0,0 +1,5 @@
+#map with lambda
+number = [1,2,3,4,5]
+result=list(map(lambda x : x*2,number))
+print(result)
+
